6d2e4498f21c1aa5b2f3334327673e8e4895b1f9
ashutoshmaurya1122 <mauryaashutosh642@gmail.com>
2021-10-28 19:20:13 +0530

    First commit

---
+++ forces.py
@@ -0,0 +1,45 @@
+
+
+import os
+import sys
+import pandas as pd
+import openpyxl
+
+
+forces_file = "{}\\postProcessing\\forces\\0\\force.dat".format(os.getcwd())
+
+if not os.path.isfile(forces_file):
+        print("Forces file not found at "+forces_file)
+        print( "Be sure that the case has been run and you have the right directory!")
+        print ("Exiting.")
+        sys.exit()
+
+time = []
+forcex = []
+forcey = []
+forcez = []
+
+with open(forces_file,"r") as datafile:
+        for line in datafile:
+                if line[0] == "#":
+                        continue
+                data=line.split(' ')
+                time += [float(data[0])]
+                try:
+                        result = line[line.find('(')+1:line.find(')')]
+                       
+                        result=list(result.split(" "))
+                        forcex.append(result[0])
+                        forcey.append(result[1])
+                        forcez.append(result[2])
+                        
+                except:
+                        print("no file")
+                        continue
+                
+datafile.close()
+
+d ={ 'Time' : pd.Series(time,dtype=float),'x force' : pd.Series(forcex,dtype=float),'y force' : pd.Series(forcey,dtype=float),'z force' : pd.Series(forcez,dtype=float)}
+df= pd.DataFrame(d)
+#print(df)
+df.to_excel('force.xlsx', sheet_name='new_sheet_name')
---
+++ moment.py
@@ -0,0 +1,45 @@
+
+
+import os
+import sys
+import pandas as pd
+import openpyxl
+
+
+forces_file = "{}\\postProcessing\\forces\\0\\moment.dat".format(os.getcwd())
+
+if not os.path.isfile(forces_file):
+        print("Forces file not found at "+forces_file)
+        print( "Be sure that the case has been run and you have the right directory!")
+        print ("Exiting.")
+        sys.exit()
+
+time = []
+momentx = []
+momenty = []
+momentz = []
+
+with open(forces_file,"r") as datafile:
+        for line in datafile:
+                if line[0] == "#":
+                        continue
+                data=line.split(' ')
+                time += [float(data[0])]
+                try:
+                        result = line[line.find('(')+1:line.find(')')]
+                       
+                        result=list(result.split(" "))
+                        momentx.append(result[0])
+                        momenty.append(result[1])
+                        momentz.append(result[2])
+                        
+                except:
+                        print("no file")
+                        continue
+                
+datafile.close()
+
+d ={ 'Time' : pd.Series(time,dtype=float),'x moment' : pd.Series(momentx,dtype=float),'y moment' : pd.Series(momenty,dtype=float),'z moment' : pd.Series(momentz,dtype=float)}
+df= pd.DataFrame(d)
+#print(df)
+df.to_excel('moment.xlsx', sheet_name='new_sheet_name')
