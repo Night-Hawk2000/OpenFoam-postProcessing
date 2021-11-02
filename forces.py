7a4aa767d8e234d0d7ef9ebc7f4485b6bc38f304
ASHUTOSH MAURYA <41411168+ashutoshmaurya1122@users.noreply.github.com>
2021-11-02 21:29:25 +0530

    Update forces.py
    Removed hard coading

--- forces.py
+++ forces.py
@@ -6,7 +6,7 @@ import pandas as pd
 import openpyxl
 
 
-forces_file = "{}\\postProcessing\\forces\\0\\force.dat".format(os.getcwd())
+forces_file = os.path.join(os.getcwd()+ os.sep,"postProcessing" + os.sep ,"forces" +os.sep,"0" +os.sep,"force.dat")
 
 if not os.path.isfile(forces_file):
         print("Forces file not found at "+forces_file)
